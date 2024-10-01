a657bb98c2fcd2328b4f719606945c1a62f4cf0d
Suma249 <3584_suma@katalystindia.org>
2024-10-01 15:07:22 +0530

    structured the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ImageCard from "./Components/ImageCard";
 
 function App() {
   const [images, setImages]=useState([]);
@@ -6,47 +7,26 @@ function App() {
   const [term, setTerm]=useState('');
 
   useEffect( ()=>{
-    fetch(`https://pixabay.com/api/?key=${process.eventNames.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
     .then( res => res.json())
     .then( data => {
+     // console.log(data);
       setImages(data.hits)
       setIsLoading(false)
     })
     .catch( error => console.log("error: "+error))
-  })
+  }, [])
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://unsplash.com/random" alt="" className="w-full"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            40000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            300
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            400
-          </li>
-        </ul>
+    <div className="container mx-auto">
+      {isLoading?<h1 className="text-6xl text-center mx-auto mt-32">Loading.....</h1>:
+        <div className="grid grid-cols-3 gap-4">
+        {
+          images.map (image =>{
+            <ImageCard key={image.id} image={image}/>
+          })
+        }
       </div>
-      <div className="px-6 py-4">
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-        #tag1
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-        #tag2
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-        #tag3
-        </span>
-      </div>
+     }
     </div>
   );
 }
