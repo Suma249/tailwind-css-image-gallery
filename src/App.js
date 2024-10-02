1b9c46a7352a134525468c145f5e3c577d61e4c0
Suma249 <3584_suma@katalystindia.org>
2024-10-02 10:41:51 +0530

    fixed the bug in rendering the imagecard component and added image search component code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./Components/ImageCard";
+import ImageSearch from "./Components/ImageSearch";
 
 function App() {
   const [images, setImages]=useState([]);
@@ -15,15 +16,18 @@ function App() {
       setIsLoading(false)
     })
     .catch( error => console.log("error: "+error))
-  }, [])
+  }, [term])
   return (
     <div className="container mx-auto">
+      <ImageSearch searchText={ text => setTerm(text) }/>
+
+        {
+          !isLoading && images.length ===0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>
+        }
       {isLoading?<h1 className="text-6xl text-center mx-auto mt-32">Loading.....</h1>:
         <div className="grid grid-cols-3 gap-4">
         {
-          images.map (image =>{
-            <ImageCard key={image.id} image={image}/>
-          })
+          images.map (image =><ImageCard key={image.id} image={image}/>)
         }
       </div>
      }
